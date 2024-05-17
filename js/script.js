40e0416a408292a294fe6b0a255aa8301a2f5db4
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-17 16:44:04 +0200

    Aggiunta funzionalità per rimuovere tutte le classi 'active' e svuotare il console.log grazie all'ascolto del bottone play in click.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const sectionGridAppend = document.querySelector("section#grid");
 
+const buttonAppend = document.querySelector("button#new-match")
+
 for(let i = 0; i < 100; i++){
     const articleEl = document.createElement("article");
     articleEl.classList.add("square");
@@ -11,4 +13,13 @@ for(let i = 0; i < 100; i++){
         articleEl.classList.add("active");
         console.log(`This is the cell number: ${[i + 1]}`)
     })
+
+    buttonAppend.addEventListener("click",function(){
+    articleEl.classList.remove("active");
+    console.clear();
+})
+
 }
+
+
+
