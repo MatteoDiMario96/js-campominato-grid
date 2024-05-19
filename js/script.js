676f3372b3a230405b7c4ffb2afc87e96f7ad4bf
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-19 17:17:24 +0200

    Provato a fare bonus ma non mi funzona innerHTML. Non mi sostituisce nodo o blocco che voglio io. Credo ch se riuscivo a usare quello facevo tutto quanto.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,50 @@
 const sectionGridAppend = document.querySelector("section#grid");
+const mainAppend = document.querySelector("main");
+
 
 const buttonAppend = document.querySelector("button#new-match")
+const selectHardAppend = document.querySelector("#select-hard");
+const selectVeryHardAppend = document.querySelector("#select-very-hard");
+const selectCrazyAppend = document.querySelector("#select-crazy");
 
-for(let i = 0; i < 100; i++){
-    const articleEl = document.createElement("article");
-    articleEl.classList.add("square");
-    articleEl.append([i + 1])
-    sectionGridAppend.appendChild(articleEl)
-
-
-    articleEl.addEventListener("click", function(){
-        articleEl.classList.add("active");
-        console.log(`This is the cell number: ${[i + 1]}`)
-    })
-
-    buttonAppend.addEventListener("click",function(){
-    articleEl.classList.remove("active");
-    console.clear();
+selectHardAppend.addEventListener("click",function(){
+    document.sectionGridAppend.innerHTML = `${getSquare(100)}`;
+})
+selectVeryHardAppend.addEventListener("click",function(){
+    getSquare(81);
+})
+selectCrazyAppend.addEventListener("click",function(){
+    getSquare(49);
 })
 
+getSquare(100);
+// document.querySelector("section#grid").innerHTML = getSquare(81);
+// console.log(getSquare(49))
+
+function getSquare (number){
+    for(let i = 0; i < number; i++){
+        const articleEl = document.createElement("article");
+        articleEl.classList.add("square");
+        articleEl.append([i + 1])
+        sectionGridAppend.appendChild(articleEl)
+    
+    
+        articleEl.addEventListener("click", function(){
+            articleEl.classList.add("active");
+            console.log(`This is the cell number: ${[i + 1]}`)
+        })
+        //Provare a mettere inner.HTML
+        // document.articleEl.innerHTML = articleEl Da vedere
+    //     
+    buttonAppend.addEventListener("click",function(){
+        articleEl.classList.remove("active");
+        console.clear();
+    // document.querySelector("section#grid").innerHTML  getSquare(100);
+    })
+
+
+
+    }
 }
 
 
